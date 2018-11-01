3d4e6c5d07b1fd19af5cfc7261836e198ca2e141
Anderson Chen <ander.chen00@gmail.com>
2018-11-01 16:49:25 -0300

    Added card layout for cocktail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # config.assets.css_compressor = :sass
+  config.assets.js_compressor = :uglifier
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
